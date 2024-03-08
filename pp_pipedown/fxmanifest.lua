781a572474a887b1a8cfa032df01ed0e0e3e359c
EP <ep.kouhia@gmail.com>
2024-03-09 01:23:54 +0200

    Audio Occlusions

--- pp_pipedown/fxmanifest.lua
+++ pp_pipedown/fxmanifest.lua
@@ -8,7 +8,10 @@ author 'EP'
 version '1.0'
 
 files {
-    "stream/pp_pipedown_props.ytyp"
+    "stream/pp_pipedown_props.ytyp",
+    'audio/BF4599EA_game.dat151.rel',
+    'stream/1086001626.ymt'
 }
 
 data_file 'DLC_ITYP_REQUEST' 'stream/pp_pipedown_props.ytyp'
+data_file 'AUDIO_GAMEDATA' 'audio/BF4599EA_game.dat'
