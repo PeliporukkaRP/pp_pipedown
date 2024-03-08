46f936eb6d34abe685b218afc85d99ceee6536db
EP <ep.kouhia@gmail.com>
2024-03-08 15:01:46 +0200

    Init

---
+++ pp_pipedown/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+this_is_a_map 'yes'
+
+description 'Pipedown Cigarshop - Mirror Park'
+author 'EP'
+version '1.0'
+
+files {
+    "stream/pp_pipedown_props.ytyp"
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/pp_pipedown_props.ytyp'
